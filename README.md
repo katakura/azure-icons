097b4e4cb6a01d5888a6a7402c09d27e8df0c961
Yoshimasa Katakura <yoshimasa.katakura@outlook.com>
2023-03-29 23:01:25 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Azure architecture icons V13 (png version)
+
+## Introduction
+
+This repository is Microsoft's official Azure icon set converted to png format in various sizes.
+
+The original svg format file can be downloaded from the following link
+
+[https://learn.microsoft.com/en-us/azure/architecture/icons/](https://learn.microsoft.com/en-us/azure/architecture/icons/)
+
+## how to use
+
+Various sizes of png files are stored under the "icons" directory and can be used freely.
+
+**For more information on terms and conditions of use, please check the official website above.**
+
+A release file in zip format is also provided.
+
+This repository contains the following pixel sizes
+
+- 64px
+- 256px
+- 512px
+
+If you want to use png files of other sizes, create them by adjusting the "GENERATE_SIZE" environment variable in download.sh.
+
+It can be run in a Linux environment with curl and inkscape already installed.
---
+++ download.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -eu -o pipefail
+
+DOWNLOAD_URL=https://arch-center.azureedge.net/icons/Azure_Public_Service_Icons_V13.zip
+GENERATE_SIZE=(64 256 512)
+ZIP_FILE_NAME=azure-icons.zip
+TMP_DIR=./tmp
+LIST_FILE=icon-files.csv
+ICON_DIR=./icons
+
+rm -rf ${TMP_DIR}
+mkdir -p ${TMP_DIR}
+
+# download original icon file
+echo "### download original file"
+curl -s -o ${TMP_DIR}/${ZIP_FILE_NAME} ${DOWNLOAD_URL}
+sh -c "cd ${TMP_DIR}; unzip ${ZIP_FILE_NAME}" > /dev/null
+
+# find target SVG files
+echo "### generating list file"
+rm -f ${TMP_DIR}/${LIST_FILE}
+find ./tmp -name \*.svg -print | xargs -i sh -c "echo \`dirname \"{}\" | sed -r \"s/.+Icons\///g\" | sed \"s/ + /+/\" | sed \"s/ /_/g\" \`,\`basename \"{}\" | cut -d- -f4-100 | cut -d. -f1 | sed \"s/-/_/g\" | sed \"s/ //g\"\`,\"{}\"" >> ${TMP_DIR}/${LIST_FILE}
+
+# convert SVG to PNG
+for size in "${GENERATE_SIZE[@]}"; do
+    echo "### generating ${size}px icons"
+
+    while read line
+    do
+        category=$(echo ${line} | cut -d , -f 1)
+        basename=$(echo ${line} | cut -d , -f 2)
+        svgfile=$(echo ${line} | cut -d , -f 3)
+        echo "-> ${ICON_DIR}/${size}px/${category}/${basename}.png"
+        mkdir -p ${ICON_DIR}/${size}px/${category}
+        inkscape -w ${size} "${svgfile}" --export-filename ${ICON_DIR}/${size}px/${category}/${basename}.png
+
+    done < ${TMP_DIR}/${LIST_FILE}
+
+done
